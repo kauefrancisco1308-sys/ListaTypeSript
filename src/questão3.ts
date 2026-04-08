1d32eb64d5711eee321d59f0c367cd2e58ff10d1
Mateus <kauefrancisco1308@gmail.com>
2026-04-08 15:51:49 -0300

    subimdo projeto

---
+++ src/questão3.ts
@@ -0,0 +1,24 @@
+// Crie um programa que solicite dois números e simule um menu de uma calculadora:
+// 1 - Soma
+// 2 - Subtração
+// 3 - Multiplicação
+// 4 - Divisão
+// Use switch...Case
+
+let operacao = prompt("informe a operação: ")
+let n1 = Number(prompt("informe o primeiro numero"))
+let n2 = Number(prompt("informe o segundo numero"))
+switch (operacao){
+    case "soma":
+        alert("a soma dos numeros é de: "+(n1+n2))
+        break
+    case "Subtração":
+        alert("a Subtração dos numeros é de: "+(n1-n2))
+        break
+    case "Multiplicação":
+        alert("a Multiplicação dos numeros é de: "+(n1*n2))
+        break
+    case "Divisão":
+        alert("a Divisão dos numeros é de: "+(n1/n2))
+        break
+}
